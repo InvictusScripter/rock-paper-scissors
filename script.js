7f19b750fa9f0c70f6f1a4094dd6f1f7c1029c8f
InvictusScripter <gymboris@gmail.com>
2023-12-09 18:25:57 +0100

    Add HTML and JavaScript files for the game

--- script.js
+++ script.js
@@ -1,62 +1,60 @@
-// Step 3: Create a function called getComputerChoice
+// Get references to UI elements
+const rockButton = document.getElementById('rock');
+const paperButton = document.getElementById('paper');
+const scissorsButton = document.getElementById('scissors');
+const resultsDisplay = document.getElementById('results');
+const playerScoreDisplay = document.getElementById('playerScore');
+const computerScoreDisplay = document.getElementById('computerScore');
+
+// Initialize game variables
+let playerScore = 0;
+let computerScore = 0;
+
+// Function to play a single round of the game
+function playRound(playerSelection) {
+    const computerSelection = getComputerChoice();
+
+    // Implement game logic to determine the winner of the round
+    let roundResult = "";  // Initialize a variable to store the result message
+    
+    if (playerSelection === computerSelection) {
+        roundResult = "It's a tie! Replay the round.";
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
+        roundResult = `You win! ${playerSelection} beats ${computerSelection}.`;
+        playerScore++; // Update player score
+    } else {
+        roundResult = `You lose! ${computerSelection} beats ${playerSelection}.`;
+        computerScore++; // Update computer score
+    }
+
+    // Display the round result and running score on the UI
+    resultsDisplay.textContent = roundResult;
+    playerScoreDisplay.textContent = playerScore;
+    computerScoreDisplay.textContent = computerScore;
+
+    // Check if any player reaches a score of 5 (win condition) and announce the winner
+    if (playerScore === 5) {
+        resultsDisplay.textContent += ' Congratulations! You win the game!';
+        // Disable the buttons or add any other end-game logic here
+    } else if (computerScore === 5) {
+        resultsDisplay.textContent += ' Sorry, you lose the game.';
+        // Disable the buttons or add any other end-game logic here
+    }
+}
+
+// Event listeners for button clicks
+rockButton.addEventListener('click', () => playRound('rock'));
+paperButton.addEventListener('click', () => playRound('paper'));
+scissorsButton.addEventListener('click', () => playRound('scissors'));
+
+// Function to get computer's choice (you can reuse your existing getComputerChoice function)
 function getComputerChoice() {
-    const choices = ['Rock', 'Paper', 'Scissors'];
+    const choices = ['rock', 'paper', 'scissors'];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
 
-// Step 4: Write a function playRound
-function playRound(playerSelection, computerSelection) {
-    // Make playerSelection case-insensitive
-    playerSelection = playerSelection.toLowerCase();
-
-    // Check for a tie
-    if (playerSelection === computerSelection.toLowerCase()) {
-        return "It's a tie! Replay the round.";
-    }
-
-    // Check for winner
-    if (
-        (playerSelection === 'rock' && computerSelection === 'Scissors') ||
-        (playerSelection === 'paper' && computerSelection === 'Rock') ||
-        (playerSelection === 'scissors' && computerSelection === 'Paper')
-    ) {
-        return `You win! ${playerSelection} beats ${computerSelection}.`;
-    } else {
-        return `You lose! ${computerSelection} beats ${playerSelection}.`;
-    }
-}
-
-// Step 6: Write a new function called game
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    // Play best-of-five rounds
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("Enter your choice: Rock, Paper, or Scissors");
-        const computerSelection = getComputerChoice();
-        const roundResult = playRound(playerSelection, computerSelection);
-        
-        console.log(roundResult);
-
-        // Update scores
-        if (roundResult.includes("win")) {
-            playerScore++;
-        } else if (roundResult.includes("lose")) {
-            computerScore++;
-        }
-    }
-
-    // Determine the winner of the game
-    if (playerScore > computerScore) {
-        console.log("Congratulations! You win the game!");
-    } else if (playerScore < computerScore) {
-        console.log("Sorry, you lose the game.");
-    } else {
-        console.log("It's a tie game!");
-    }
-}
-
-// Step 8: Call the game function
-game();
